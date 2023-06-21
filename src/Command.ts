38ca36bc3c3b7f24140c93cb0f92ced542040512
6627811 <ras.roderick@gmail.com>
2023-06-21 22:32:24 +0200

    chore: added license info

--- src/Command.ts
+++ src/Command.ts
@@ -1,3 +1,11 @@
+/**
+ * This program has been developed by students from the bachelor Computer Science at Utrecht University within the Software Project course.
+ * © Copyright Utrecht University (Department of Information and Computing Sciences)
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
 import { Flags } from "./Input";
 import HashData from "./modules/searchSECO-parser/src/HashData";
 import { AuthorData } from "./modules/searchSECO-spider/src/Spider";
